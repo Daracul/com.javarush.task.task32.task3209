82204577e9ee89bfd3f6e7fd60a440499e1b3e4f
Alexey Malakhov <au.malakhov@gmail.com>
2017-06-06 17:07:32 +0300

    HTML Editor step13

--- actions/SubscriptAction.java
+++ actions/SubscriptAction.java
@@ -2,8 +2,12 @@ package com.javarush.task.task32.task3209.actions;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
+import javax.swing.text.StyleConstants;
 
 /**
  * Created by amalakhov on 06.06.2017.
@@ -12,12 +16,19 @@ public class SubscriptAction extends StyledEditorKit.StyledTextAction {
 
 
     public SubscriptAction() {
-        super(null);
+        super(StyleConstants.Subscript.toString());
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JEditorPane editor=getEditor(e);
+        if (editor!=null){
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setSubscript(simpleAttributeSet, !StyleConstants.isSubscript(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
 
     }
 }
