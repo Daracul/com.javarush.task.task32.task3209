acf4d9e0a6aa5e9f905b834201e0532803c07ecc
Alexey Malakhov <au.malakhov@gmail.com>
2017-06-07 16:35:51 +0300

    HTML Editor step23

--- Controller.java
+++ Controller.java
@@ -1,12 +1,10 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 /**
  * Created by amalakhov on 05.06.2017.
@@ -81,5 +79,25 @@ public class Controller {
     }
     public void openDocument(){}
     public void saveDocument(){}
-    public void saveDocumentAs(){}
+    public void saveDocumentAs(){
+        view.selectHtmlTab();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new HTMLFileFilter());
+        fileChooser.setDialogTitle("Save File");
+        int option = fileChooser.showSaveDialog(view);
+        if (option==JFileChooser.APPROVE_OPTION){
+            currentFile=fileChooser.getSelectedFile();
+            view.setTitle(currentFile.getName());
+            try(FileWriter fileWriter = new FileWriter(currentFile)){
+                new HTMLEditorKit().write(fileWriter,document,0,document.getLength());
+
+            }
+            catch (IOException e){ExceptionHandler.log(e);}
+            catch (BadLocationException e) {
+                ExceptionHandler.log(e);
+            }
+        }
+
+
+    }
 }
