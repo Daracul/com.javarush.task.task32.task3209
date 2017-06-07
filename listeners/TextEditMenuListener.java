5be2217143ee08b8cb27a6a9f4370cffa4fd34ed
Alexey Malakhov <au.malakhov@gmail.com>
2017-06-07 10:51:01 +0300

    HTML Editor step14

--- listeners/TextEditMenuListener.java
+++ listeners/TextEditMenuListener.java
@@ -2,8 +2,10 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 /**
  * Created by amalakhov on 06.06.2017.
@@ -17,6 +19,10 @@ public class TextEditMenuListener implements MenuListener {
 
     @Override
     public void menuSelected(MenuEvent e) {
+        JMenu menu = (JMenu) e.getSource();
+        for (Component component:menu.getMenuComponents()){
+            component.setEnabled(view.isHtmlTabSelected());
+        }
 
     }
 
